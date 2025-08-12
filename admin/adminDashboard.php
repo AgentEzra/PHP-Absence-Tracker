f217a35f01e0c0e5eaa3dfa66bc1c7ef6a8fbbda
Zr... <166925759+AgentEzra@users.noreply.github.com>
2025-08-12 17:26:35 +0700

    dashboard
    dashboard

---
+++ admin/adminDashboard.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrator</title>
+    <link rel="stylesheet" href="./styleAdmin.css">
+</head>
+<body>
+    <h1>Manage Creds Database</h1>
+    <a href="./creds/read.php">Pergi ke creds database?</a>
+
+    <h1>Manage Absence Database</h1>
+    <a href="./absence/read.php">Pergi ke absence database?</a>
+</body>
+</html>
